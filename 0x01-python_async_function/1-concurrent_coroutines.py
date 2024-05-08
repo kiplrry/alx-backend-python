87f0a696f59f53676234088f32e1852f5f2d3910
kiplrry <kiplarry19@gmail.com>
2024-05-08 14:43:26 +0300

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+this module implements write an async routine
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    akes in 2 int arguments (in this order): n and max_delay.
+    it will spawn wait_random n times with the specified max_delay.
+
+    wait_n should return the list of all the delays (float values).
+    The list of the delays should be
+    in ascending order without using sort() because of concurrency.
+    """
+    i = 0
+    arr = []
+    while i < n:
+        ans = await wait_random(max_delay)
+        arr.append(ans)
+        i += 1
+
+    return arr
