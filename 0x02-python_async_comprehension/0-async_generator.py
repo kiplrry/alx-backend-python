42c449f2b3477f3f54a5f9e8a2fc02ff90037e3c
kiplrry <kiplarry19@gmail.com>
2024-05-10 22:50:05 +0300

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ module for the asyc generator
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async generator
     """
