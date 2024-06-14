61c89a2db0b886c2413d42333185e22eea4ac146
kiplrry <kiplarry19@gmail.com>
2024-06-14 12:57:02 +0300

    100-safe_first_element.py

--- 0x00-python_variable_annotations/test.py
+++ 0x00-python_variable_annotations/test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-make_multiplier = __import__('8-make_multiplier').make_multiplier
-print(make_multiplier.__annotations__)
-fun = make_multiplier(2.22)
-print("{}".format(fun(2.22)))
+safe_first_element =  __import__('100-safe_first_element').safe_first_element
+
+print(safe_first_element.__annotations__)
